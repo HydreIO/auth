defb201fcc3db34dcc186b17c5e6968a53a42919
Sceat <sceat@hydre.io>
2020-02-09 16:49:55 +0100

    cookies always use a domain, LOCALHOST env var make them http

--- packages/auth-core/core/cookies.js
+++ packages/auth-core/core/cookies.js
@@ -2,12 +2,15 @@ import cookie from 'cookie'
 
 const aYear = 60 * 60 * 24 * 365 * 1
 
-// by specifying development env variable we allow http cookies for test purposes
-const prodOpt = COOKIE_DOMAIN => process.env.AUTH_LOCAL?.toLowerCase() === 'true' ? {} : { domain: COOKIE_DOMAIN, secure: true }
-const cookieOpt = { httpOnly: true, secure: false, sameSite: 'Strict', path: '/' }
+const cookieOpt = {
+	httpOnly: true,
+	secure: process.env.LOCALHOST?.toLowerCase() === 'true',
+	sameSite: 'Strict',
+	path: '/'
+}
 
 export const accessCookie = ({ COOKIE_DOMAIN, ACCESS_COOKIE_NAME }) => rememberMe => accessToken => {
-	const payload = { ...cookieOpt, ...prodOpt(COOKIE_DOMAIN) }
+	const payload = { ...cookieOpt, domain: COOKIE_DOMAIN }
 
 	// 1 years cookie :) that's a lot of cookies
 	// in case the user choose not be remembered, we don't set a expiration
@@ -21,16 +24,16 @@ export const accessCookie = ({ COOKIE_DOMAIN, ACCESS_COOKIE_NAME }) => rememberM
 }
 
 export const refreshCookie = ({ COOKIE_DOMAIN, REFRESH_COOKIE_NAME }) => refreshToken => {
-	const payload = { ...cookieOpt, maxAge: aYear, ...prodOpt(COOKIE_DOMAIN) }
+	const payload = { ...cookieOpt, maxAge: aYear, domain: COOKIE_DOMAIN }
 	return cookie.serialize(REFRESH_COOKIE_NAME, refreshToken, payload)
 }
 
 export const expiredAccessCookie = ({ COOKIE_DOMAIN, ACCESS_COOKIE_NAME }) => {
-	const payload = { ...cookieOpt, expires: new Date(0), ...prodOpt(COOKIE_DOMAIN) }
+	const payload = { ...cookieOpt, expires: new Date(0), domain: COOKIE_DOMAIN }
 	return cookie.serialize(ACCESS_COOKIE_NAME, 'hehe boi', payload)
 }
 
 export const expiredRefreshCookie = ({ COOKIE_DOMAIN, REFRESH_COOKIE_NAME }) => {
-	const payload = { ...cookieOpt, expires: new Date(0), ...prodOpt(COOKIE_DOMAIN) }
+	const payload = { ...cookieOpt, expires: new Date(0), domain: COOKIE_DOMAIN }
 	return cookie.serialize(REFRESH_COOKIE_NAME, 'hehe boi', payload)
 }
