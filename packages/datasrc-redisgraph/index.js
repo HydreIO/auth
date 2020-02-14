7c0722e418052a968a6d93b949b1bff361c0c7f3
Sceat <sceat@hydre.io>
2020-02-14 16:49:17 +0100

    start lib implem

--- packages/datasrc-redisgraph/index.js
+++ packages/datasrc-redisgraph/index.js
@@ -1,138 +1,55 @@
 import { Graph } from 'redisgraph.js'
-import { map, concat, flatMap, concatMap, tap, toArray, reduce } from 'rxjs/operators'
-import { from, zip, defer, of } from 'rxjs'
 import neo4j from 'neo4j-driver'
-import redis from 'redis'
-import { promisify, isArray, inspect } from 'util'
+import { from, zip } from 'rxjs'
+import { toArray, reduce, map } from 'rxjs/operators'
 
 const debug = 'graph' |> require('debug')('auth').extend
 const graph = new Graph('test', 6379, '127.0.0.1')
 
-const RESULT_TYPE = {
-  UNKNOWN: 0,
-  SCALAR: 1,
-  NODE: 2,
-  RELATION: 3
-}
-
-const CC = ['UNKNOWN', 'SCALAR', 'NODE', 'RELATION']
-
-const DATA_TYPE = {
-  UNKNOWN: 0,
-  NULL: 1,
-  STRING: 2,
-  INTEGER: 3,
-  BOOLEAN: 4,
-  DOUBLE: 5,
-  ARRAY: 6,
-  EDGE: 7,
-  NODE: 8,
-  PATH: 9
-}
-
 export default () => ({
   connect: async () => {
 
-    const client = redis.createClient(6379, '127.0.0.1')
-    const sendCmd = promisify(client.send_command).bind(client)
+    const foo = async (...a) => {
+      const [raw, ...args] = a
+      args.push('')
 
-    client.on("error", function (error) {
-      console.error(error);
-    });
-
-    const cachedKeys = procedure => graphId => {
-      const monad = { keys: [] }
-      const fetch = async () => {
-        const toYield = procedure.slice(3, -3)
-        const args = [graphId, `CALL ${procedure} YIELD ${toYield} RETURN ${toYield} SKIP ${monad.keys.length}`]
-        const [, missing] = await sendCmd('graph.QUERY', args)
-        if (missing.some(a => a.length > 1)) throw new Error('This version of the driver does not support multi labels on nodes :shrug:')
-        monad.keys = [...monad.keys, ...missing.flat()]
-      }
-      return async index => {
-        index >= monad.keys.length && await fetch()
-        return monad.keys[index]
-      }
-    }
-
-    const labels = cachedKeys('db.labels()')('test')
-    const relationsKeys = cachedKeys('db.relationshipTypes()')('test')
-    const propertyKeys = cachedKeys('db.propertyKeys()')('test')
-
-    const parseScalar$ = ([type, value]) => {
-      switch (type) {
-        case DATA_TYPE.UNKNOWN:
-        case DATA_TYPE.NULL:
-          return of(undefined)
-        case DATA_TYPE.DOUBLE:
-          return of(+value)
-        case DATA_TYPE.BOOLEAN:
-          return of(value === 'true')
-        case DATA_TYPE.ARRAY:
-          return from(value).pipe(flatMap(parseScalar$))
-        case DATA_TYPE.NODE:
-          return parseNode$(value)
-        case DATA_TYPE.EDGE:
-          return parseEdge$(value)
-        case DATA_TYPE.PATH:
-          return parsePath$(value)
-        default: return of(value)
-      }
-    }
-    const parseProp$ = ([key, type, value]) => defer(async () => [await propertyKeys(key), await parseScalar$([type, value]).toPromise()])
-    const parseProps$ = props => from(props).pipe(concatMap(parseProp$), toArray(), map(Object.fromEntries))
-    const parseNode$ = ([id, nodeLabels, props]) => defer(async () => ({
-      id,
-      labels: await from(nodeLabels).pipe(concatMap(i => from(labels(i))), toArray()).toPromise(),
-      properties: await parseProps$(props).toPromise()
-    }))
-    const parseEdge$ = ([id, type, srcNodeId, destNodeId, props]) => defer(async () => ({
-      id,
-      label: await relationsKeys(type),
-      srcNodeId,
-      destNodeId,
-      properties: await parseProps$(props).toPromise()
-    }))
-    const parsePathRow$ = path => from(path).pipe(concatMap(parseScalar$), toArray())
-    const parsePath$ = ([[, nodes], [, edges]]) => defer(async () => ({
-      nodes: await parsePathRow$(nodes).toPromise(),
-      edges: await parsePathRow$(edges).toPromise()
-    }))
-
-    // const query = `MERGE (a:Test:Tested {who:"-> a",uuid: 981.5, foo:'bar'})-[b:FOO {who:"-> b",since: 'a long time ago'}]->(c:OK {who:"-> c", arr:['one',2,'three']}) RETURN a.foo,b,c`
-    await sendCmd('graph.QUERY', ['test', `CREATE (a:Test {who:"-> a",uuid: 981.5, foo:'bar'})-[b:FOO {who:"-> b",since: 'a long time ago'}]->(c:OK {who:"-> c", arr:['one',2,'three']})`])
-    const query = `MATCH path = ()-[]->() RETURN path`
-
-    from(sendCmd('graph.QUERY', ['test', query, '--compact']))
-      .pipe(
-        tap(() => debug.extend('query')('%s', query)),
-        tap(([, , stats]) => debug.extend('stats')(stats)),
-        flatMap(([header, [cell]]) => zip(from(header), from(cell))),
-        concatMap(([[cellType, label], cell]) => defer(async () => {
-          switch (cellType) {
-            case RESULT_TYPE.NODE:
-              return { label, value: await parseNode$(cell).toPromise() }
-            case RESULT_TYPE.RELATION:
-              return { label, value: await parseEdge$(cell).toPromise() }
-            case RESULT_TYPE.SCALAR:
-              return { label, value: await parseScalar$(cell).toPromise() }
-            case RESULT_TYPE.UNKNOWN:
-              throw new Error(`The cell of type ${cellType} is unkown billy, the end is near.. get cover!`)
-          }
-        }))
-      )
-      .subscribe({
-        next(a) {
-          debug.extend('result')('%s', inspect(a, false, null, true))
-        },
-        async complete() {
-          debug('deleting graph..')
-          await sendCmd('graph.DELETE', ['test'])
-          debug('Bye.')
-          process.exit(0)
+      const isDouble = n => 
+      const respifyValue = v => {
+        if (!v) return 'NULL'
+        switch (typeof v) {
+          case 'boolean':
+            return v ? 'true' : 'false'
+          case 'number':
+            return Number.isSafeInteger(v) ? v : `${v}`
+          case 'bigint':
+            return `${v}`
+            case 'function':
+            case 'symbol':
+              return undefined
         }
-      })
+      }
 
+      await zip(from(raw), from(args).pipe(map(v => {
+
+      })))
+        .pipe(reduce((acc, [a, b]) => [...acc, a, b], []), map(a => a.join('')))
+        .toPromise().then(a => console.log(a))
+    }
+
+    const user = { uuid: 'xxx-xx-xxx', name: 'Tony' }
+    await foo`MERGE (foo:User ${user})-[:Knows]->(thanos { name: 'Thanos', age: ${5 + 5}, a: ${true}, b: ${922337203685477580750}, c: ${51.0000000000000016} }) RETURN foo AS user, thanos`
+
+    process.exit(0)
+
+    const driver = rgraph(client)
+    const graphA = driver('aGraph')
+    const graphB = drive('anotherGraph')
+
+
+    const result$ = graphA`MERGE (foo:User ${user})-[:Knows]->(thanos { name: 'Thanos', age: ${5 + 5} }) RETURN foo AS user, thanos`
+    result$.pipe(
+      tap(({ stats }) => console.log(stats))
+    ).subscribe(({ user, thanos }) => console.log(user.name)) // print Tony
 
     // const driver = neo4j.driver(process.env.NEO4J_URI, neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PWD), { encrypted: "ENCRYPTION_ON" })
     // const rxs = driver.rxSession()
