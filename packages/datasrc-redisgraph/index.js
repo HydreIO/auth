052ef8ecbeb99ac42e803dc0940c014516873a91
Sceat <sceat@hydre.io>
2020-02-14 17:59:40 +0100

    testing graph library

--- packages/datasrc-redisgraph/index.js
+++ packages/datasrc-redisgraph/index.js
@@ -1,55 +1,25 @@
-import { Graph } from 'redisgraph.js'
-import neo4j from 'neo4j-driver'
 import { from, zip } from 'rxjs'
 import { toArray, reduce, map } from 'rxjs/operators'
+import rgraph from '@hydre/rgraph'
+import redis from 'redis'
+import { inspect } from 'util'
 
 const debug = 'graph' |> require('debug')('auth').extend
-const graph = new Graph('test', 6379, '127.0.0.1')
 
 export default () => ({
   connect: async () => {
 
-    const foo = async (...a) => {
-      const [raw, ...args] = a
-      args.push('')
-
-      const isDouble = n => 
-      const respifyValue = v => {
-        if (!v) return 'NULL'
-        switch (typeof v) {
-          case 'boolean':
-            return v ? 'true' : 'false'
-          case 'number':
-            return Number.isSafeInteger(v) ? v : `${v}`
-          case 'bigint':
-            return `${v}`
-            case 'function':
-            case 'symbol':
-              return undefined
-        }
-      }
-
-      await zip(from(raw), from(args).pipe(map(v => {
-
-      })))
-        .pipe(reduce((acc, [a, b]) => [...acc, a, b], []), map(a => a.join('')))
-        .toPromise().then(a => console.log(a))
-    }
-
     const user = { uuid: 'xxx-xx-xxx', name: 'Tony' }
-    await foo`MERGE (foo:User ${user})-[:Knows]->(thanos { name: 'Thanos', age: ${5 + 5}, a: ${true}, b: ${922337203685477580750}, c: ${51.0000000000000016} }) RETURN foo AS user, thanos`
+    const graph = rgraph(redis.createClient('redis://127.0.0.1:6379/'))('test')
+    // const result$ = graph`MERGE (foo:User ${user})-[:Knows]->(thanos { name: 'Thanos', age: ${5 + 5} }) RETURN foo AS user, thanos`
+    // const result$ = graph`MERGE (foo:User ${user})-[:Knows]->(thanos { name: 'Thanos', age: ${5 + 5}, a: ${true}, b: ${922337203685477580750}, c: ${51.000000000016} }) RETURN foo AS user, thanos`
+    const result$ = graph`MERGE (foo:User ${user}) RETURN foo`
 
+    console.log(inspect(await result$.toPromise(), false, null, true))
     process.exit(0)
-
-    const driver = rgraph(client)
-    const graphA = driver('aGraph')
-    const graphB = drive('anotherGraph')
-
-
-    const result$ = graphA`MERGE (foo:User ${user})-[:Knows]->(thanos { name: 'Thanos', age: ${5 + 5} }) RETURN foo AS user, thanos`
-    result$.pipe(
-      tap(({ stats }) => console.log(stats))
-    ).subscribe(({ user, thanos }) => console.log(user.name)) // print Tony
+    // result$.pipe(
+    //   tap(({ stats }) => console.log(stats))
+    // ).subscribe(({ user, thanos }) => console.log(user.name)) // print Tony
 
     // const driver = neo4j.driver(process.env.NEO4J_URI, neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PWD), { encrypted: "ENCRYPTION_ON" })
     // const rxs = driver.rxSession()
