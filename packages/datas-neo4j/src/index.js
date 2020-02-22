e7bf4f7d8c26eb4983181368ebefd9c91a586f94
Sceat <sceat@localhost.localdomain>
2020-02-22 21:26:58 +0100

    using neo4j driver

--- packages/datas-neo4j/src/index.js
+++ packages/datas-neo4j/src/index.js
@@ -1,5 +1,5 @@
 import { interval } from 'rxjs'
-import { concatMapTo, map } from 'rxjs/operators'
+import { map, concat } from 'rxjs/operators'
 import neo4j from 'neo4j-driver'
 
 const debug = require('debug')('auth').extend('graph')
@@ -9,23 +9,51 @@ export default ({ uri, graph }) => {
   return {
     connect: async () => { driver = neo4j.driver(uri) },
     crud: {
-      fetchByUid: async uuid => run`MATCH (user:User {uuid: ${uuid}}) RETURN user`.pipe(map(extractUser)).toPromise(),
-      fetchByMail: async mail => run`MATCH (user:User {mail: ${mail}}) RETURN user`.pipe(map(extractUser)).toPromise(),
+      fetchByUid: async uuid => {
+        const rxSession = driver.rxSession()
+        return rxSession.run('MATCH (user:User {uuid: $uuid}) RETURN user', { uuid })
+          .records()
+          .pipe(
+            map(records => records.get('user')),
+            concat(rxSession.close())
+          ).toPromise()
+
+      },
+      fetchByMail: async mail => {
+        const rxSession = driver.rxSession()
+        return rxSession.run('MATCH (user:User {mail: $mail}) RETURN user', { mail })
+          .records()
+          .pipe(
+            map(records => records.get('user')),
+            concat(rxSession.close())
+          ).toPromise()
+      },
       pushByUid: async (uuid, user) => {
         const { sessions, ...userWithoutSessions } = user
-        await run`MERGE (user:User {uuid: ${uuid}})
-                  WITH user
-                  SET ${plusEquals('user', userWithoutSessions)}
-                  WITH ${sessions} as userSessions
-                  UNWIND userSessions AS userSession
-                  MERGE (user)-[:HAS_SESSION]->(mergedSession:Session { hash: userSession.hash })
-                  SET mergedSession += userSession
-                  WITH DISTINCT user, collect(newSession.hash) as newSessionHash
-                  MATCH (user)-->(s:Session)
-                  WHERE NOT s.hash IN newSessionHash
-                  DELETE s`.toPromise()
+        const rxSession = driver.rxSession()
+        return rxSession.run(
+          `MERGE (user:User {uuid: ${uuid}})
+           WITH user
+           SET user += $user
+           WITH $sessions as userSessions
+           UNWIND userSessions AS userSession
+           MERGE (user)-[:HAS_SESSION]->(mergedSession:Session { hash: userSession.hash })
+           SET mergedSession += userSession
+           WITH DISTINCT user, collect(newSession.hash) as newSessionHash
+           MATCH (user)-->(s:Session)
+           WHERE NOT s.hash IN newSessionHash
+           DETACH DELETE s`, { user: userWithoutSessions, sessions })
+          .records().pipe(concat(rxSession.close())).toPromise()
       },
-      existByMail: async mail => run`MATCH (user:User {mail: ${mail}}) RETURN user`.pipe(map(result => !!result?.user)).toPromise()
+      existByMail: async mail => {
+        const rxSession = driver.rxSession()
+        return rxSession.run('MATCH (user:User {mail: $mail}) RETURN user', { mail })
+          .records()
+          .pipe(
+            map(records => !!records.get('user')),
+            concat(rxSession.close())
+          ).toPromise()
+      }
     }
   }
 }
