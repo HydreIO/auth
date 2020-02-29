64095a3279b13561ab73ecd27d619bd47fa6b533
Sceat <sceat@hydre.io>
2020-02-29 18:32:27 +0100

    fix mongo uri

--- packages/auth-server-mongo/src/index.js
+++ packages/auth-server-mongo/src/index.js
@@ -1,7 +1,7 @@
 import mongoConnector from '@hydre/datas-mongo'
 import authServer from '@hydre/auth-server-core'
 
-const { MONGO_URI = 'localhost:27017', COLLECTION = 'users', DATABASE = 'authentication' } = process.env
+const { MONGO_URI = 'mongodb://localhost:27017', COLLECTION = 'users', DATABASE = 'authentication' } = process.env
 
 void async function() {
   const { connect, crud } = mongoConnector({ uri: MONGO_URI, collection: COLLECTION, db: DATABASE })
